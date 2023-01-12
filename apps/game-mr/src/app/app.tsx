5b4186862c48679745d4b11be86f26bdb3a615a9
abcdmku <63693423+abcdmku@users.noreply.github.com>
2023-01-11 23:53:46 -0600

    Update app.tsx

--- apps/game-mr/src/app/app.tsx
+++ apps/game-mr/src/app/app.tsx
@@ -9,7 +9,7 @@ export function App() {
   const [user, setUser] = useState<User>({} as User);
 
   useEffect(() => {
-    console.log(user);
+    console.log("user", user);
   }, [user]);
   
   return user && user?.name === undefined ? (
